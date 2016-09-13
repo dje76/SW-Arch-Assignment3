13cb6104110862f1eff7fe7849040dbb64bcf530
dje76 <dje76@msstate.edu>
2016-09-13 13:46:54 -0500

    updating cart and user

--- OnlineShopping/OnlineShopping/Cart.h
+++ OnlineShopping/OnlineShopping/Cart.h
@@ -12,6 +12,7 @@ public:
 	Cart(void);
 	void addToCart(Item item, int num);
 	void removeFromCart(Item item, int num);
+	void clear();
 };
 
 //Constructor
@@ -59,6 +60,12 @@ void Cart::removeFromCart(Item item, int num){
 	//dbManager.updateCartInDatabase(items, quantity);
 }
 
+void Cart::clear(){
+	items.clear();
+	quantity.clear();
+	//dbManager.updateCartInDatabase(items, quantity);
+}
+
 //the Order class, it inharits from Cart
 class Order : public Cart{
 	public:
@@ -66,10 +73,6 @@ class Order : public Cart{
 	string payment;
 
 	Order(void);
-	void Cancel();
-	void Confirm();
-	void updateAddress(string newAddress);
-	void updatePayment(string newPayment);
 };
 
 Order::Order(void){
@@ -77,29 +80,4 @@ Order::Order(void){
 	for(int i=0;i<items.size();i++){
 		total += items[i].price*this->quantity[i];
 	}
-}
-
-//sets the contents of the cart to be empty
-void Order::Cancel(){
-	items.clear();
-	quantity.clear();
-	//dbManager.updateCartInDatabase(items, quantity);
-}
-
-//stores the order in the database and empties the cart
-void Order::Confirm(){
-	//dbManager.addOrderToCart(items, quantity, total, address, payment);
-	items.clear();
-	quantity.clear();
-	//dbManager.updateCartInDatabase(items, quantity);
-}
-
-//updates the shipping address
-void Order::updateAddress(string newAddress){
-	address = newAddress;
-}
-
-//updates the payment info
-void Order::updatePayment(string newPayment){
-	payment = newPayment;
 }
--- OnlineShopping/OnlineShopping/User.h
+++ OnlineShopping/OnlineShopping/User.h
@@ -6,14 +6,16 @@ Consists of User and Admin functionality.
 class User {
 public:
 	string name;
-	Order order;
+	Cart cart;
+	string payment;
+	string address;
+	//Database dbManager;
 
 	User();
 	User(string name);
 	void Login(Database db);
 	void set_name(string n);
 	void purchase_items();
-	void cancel_order();
 	void updateAddress(string newAddress);
 	void updatePayment(string newPayment);
 	void addToCart(Item item, int num);
@@ -26,26 +28,24 @@ User::User(string name) {	this->name = name; }
 //setter
 void User::set_name(string n) { this->name = n; }
 
+//stores the order in the database and empties the cart
 void User::purchase_items(){
-	order.Confirm(name);
-}
-
-void User::cancel_order(){
-	order.Cancel();
+	//dbManager.addOrderToDatabase(name, cart.items, cart.quantity, cart.total, address, payment);
+	cart.clear();
 }
 
 void User::updateAddress(string newAddress){
-	order.updateAddress(newAddress);
+	address = newAddress;
 }
 
 void User::updatePayment(string newPayment){
-	order.updatePayment(newPayment);
+	payment = newPayment;
 }
 
 void User::addToCart(Item item, int num){
-	order.addToCart(item, num);
+	cart.addToCart(item, num);
 }
 
 void User::removeFromCart(Item item, int num){
-	order.removeFromCart(item, num);
+	cart.removeFromCart(item, num);
 }
