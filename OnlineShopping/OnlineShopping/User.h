2e5ce0660d1c0cf4fa2637be495f0103181d9005
EvanFarry <emf134@msstate.edu>
2016-09-13 12:49:33 -0500

    Moved Operational while loop to Main.cpp

--- OnlineShopping/OnlineShopping/User.h
+++ OnlineShopping/OnlineShopping/User.h
@@ -10,48 +10,12 @@ public:
 	User();
 	User(string name);
 	void Login(Database db);
+	void set_name(string n);
 };
 
-User::User(void) {}
+User::User(void) { }
+User::User(string name) {	this->name = name; }
 
-User::User(string name) {
-	this->name = name;
-}
+//setter
+void User::set_name(string n) { this->name = n; }
 
-void User::Login(Database db) {
-	char input[100];
-	
-	cout << "Welcome, " << this->name << "!" << endl;
-	while (1) {
-		cout << "Options:" << endl;
-		cout << "1. browse" << endl;
-		cout << "2. search" << endl;
-		cout << "3. logout" << endl;
-
-		cout << this->name << ": ";
-		cin.getline(input, sizeof(input));
-
-		if (!strcmp(input, "1") || !strcmp(input, "browse")) {
-			db.print_all();
-		}
-
-		// Call search functionality
-		if (!strcmp(input, "2") || !strcmp(input, "search")) {
-			cout << "What would you like to search for?" << endl;
-			cout << this->name << ": ";
-			cin.getline(input, sizeof(input));
-			db.search_item(input);
-		}
-
-		// Exit the while loop.
-		if (!strcmp(input, "3") || !strcmp(input, "logout")) {
-			break;
-		}
-
-
-		cout << endl;
-	}
-	
-
-
-}
