f2832a32d8f720a068bae34ded4285e29a02f6cf
SkylineHorizon <skylinehorizon@hotmail.com>
2016-09-13 14:15:08 -0500

    Items Update

--- OnlineShopping/OnlineShopping/Items.h
+++ OnlineShopping/OnlineShopping/Items.h
@@ -11,7 +11,8 @@ public:
 
 	Item();
 	Item(string name, float price, int quantity, string description);
-	void print_details();
+	virtual void Item::print_details();
+	vector<string> get_details();
 };
 
 Item::Item(void) {}
@@ -27,10 +28,19 @@ void Item::print_details() {
 	cout << "       Name: " << this->name << endl;
 	cout << "      Price: " << this->price << endl;
 	cout << "   Quantity: " << this->quantity << endl;
-	cout << "Description: " << this->quantity << endl;
+	cout << "Description: " << this->description << endl;
 	cout << endl;
 }
 
+vector<string> Item::get_details() {
+	vector<string> tokens;
+	tokens.push_back("item");
+	tokens.push_back(this->name);
+	tokens.push_back(to_string(this->price));
+	tokens.push_back(to_string(this->quantity));
+	tokens.push_back(this->description);
+	return tokens;
+}
 
 class Household : public Item {
 	// "name", price, quantity, "description", "room", "size", "season"
@@ -41,7 +51,8 @@ public:
 	string season;
 
 	Household(string name, float price, int quantity, string description, string room, string size, string season);
-	void print_details();
+	void Household::print_details();
+	vector<string> get_details();
 };
 
 Household::Household(string name, float price, int quantity, string description, string room, string size, string season) {
@@ -58,11 +69,24 @@ void Household::print_details() {
 	cout << "       Name: " << this->name << endl;
 	cout << "      Price: " << this->price << endl;
 	cout << "   Quantity: " << this->quantity << endl;
-	cout << "Description: " << this->quantity << endl;
+	cout << "Description: " << this->description << endl;
 	cout << "       Room: " << this->room << endl;
 	cout << "       Size: " << this->size << endl;
 	cout << "     Season: " << this->season << endl;
 	cout << endl;
+}
+
+vector<string> Household::get_details() {
+	vector<string> tokens;
+	tokens.push_back("household");
+	tokens.push_back(this->name);
+	tokens.push_back(to_string(this->price));
+	tokens.push_back(to_string(this->quantity));
+	tokens.push_back(this->description);
+	tokens.push_back(this->room);
+	tokens.push_back(this->size);
+	tokens.push_back(this->season);
+	return tokens;
 }
 
 class Book : public Item {
@@ -75,6 +99,7 @@ public:
 
 	Book(string name, float price, int quantity, string description, string author, string genre, string isbn);
 	void print_details();
+	vector<string> get_details();
 };
 
 Book::Book(string name, float price, int quantity, string description, string author, string genre, string isbn) {
@@ -91,11 +116,24 @@ void Book::print_details() {
 	cout << "       Name: " << this->name << endl;
 	cout << "      Price: " << this->price << endl;
 	cout << "   Quantity: " << this->quantity << endl;
-	cout << "Description: " << this->quantity << endl;
+	cout << "Description: " << this->description << endl;
 	cout << "     Author: " << this->author << endl;
 	cout << "      Genre: " << this->genre << endl;
 	cout << "       ISBN: " << this->isbn << endl;
 	cout << endl;
+}
+
+vector<string> Book::get_details() {
+	vector<string> tokens;
+	tokens.push_back("book");
+	tokens.push_back(this->name);
+	tokens.push_back(to_string(this->price));
+	tokens.push_back(to_string(this->quantity));
+	tokens.push_back(this->description);
+	tokens.push_back(this->author);
+	tokens.push_back(this->genre);
+	tokens.push_back(this->isbn);
+	return tokens;
 }
 
 class Toy : public Item {
@@ -107,6 +145,7 @@ public:
 
 	Toy(string name, float price, int quantity, string description, string brand, int age);
 	void print_details();
+	vector<string> get_details();
 };
 
 Toy::Toy(string name, float price, int quantity, string description, string brand, int age) {
@@ -122,10 +161,22 @@ void Toy::print_details() {
 	cout << "       Name: " << this->name << endl;
 	cout << "      Price: " << this->price << endl;
 	cout << "   Quantity: " << this->quantity << endl;
-	cout << "Description: " << this->quantity << endl;
+	cout << "Description: " << this->description << endl;
 	cout << "      Brand: " << this->brand << endl;
 	cout << "        Age: " << this->age << "+" << endl;
 	cout << endl;
+}
+
+vector<string> Toy::get_details() {
+	vector<string> tokens;
+	tokens.push_back("toy");
+	tokens.push_back(this->name);
+	tokens.push_back(to_string(this->price));
+	tokens.push_back(to_string(this->quantity));
+	tokens.push_back(this->description);
+	tokens.push_back(this->brand);
+	tokens.push_back(to_string(this->age));
+	return tokens;
 }
 
 class Electronic : public Item {
@@ -137,6 +188,7 @@ public:
 
 	Electronic(string name, float price, int quantity, string description, string manufacturer, string category);
 	void print_details();
+	vector<string> get_details();
 };
 
 Electronic::Electronic(string name, float price, int quantity, string description, string manufacturer, string category) {
@@ -152,8 +204,20 @@ void Electronic::print_details() {
 	cout << "        Name: " << this->name << endl;
 	cout << "       Price: " << this->price << endl;
 	cout << "    Quantity: " << this->quantity << endl;
-	cout << " Description: " << this->quantity << endl;
+	cout << " Description: " << this->description << endl;
 	cout << "Manufacturer: " << this->manufacturer << endl;
-	cout <<     "Category: " << this->category << endl;
+	cout << "Category: " << this->category << endl;
 	cout << endl;
+}
+
+vector<string> Electronic::get_details() {
+	vector<string> tokens;
+	tokens.push_back("elctronic");
+	tokens.push_back(this->name);
+	tokens.push_back(to_string(this->price));
+	tokens.push_back(to_string(this->quantity));
+	tokens.push_back(this->description);
+	tokens.push_back(this->manufacturer);
+	tokens.push_back(this->category);
+	return tokens;
 }
