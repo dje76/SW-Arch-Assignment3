a6fb6eec35c0db39cc589f376bf770e708cd20c7
dje76 <dje76@msstate.edu>
2016-09-13 15:20:08 -0500

    adding get order history

--- OnlineShopping/OnlineShopping/DatabaseManager.h
+++ OnlineShopping/OnlineShopping/DatabaseManager.h
@@ -8,31 +8,41 @@ Will overwrite the text files upon completion to update information.
 
 class Database {
 public:
+	string directory;
 	vector <string> user_table;
-	vector <Household> household_table;
-	vector <Book> book_table;
-	vector <Toy> toy_table;
-	vector <Electronic> electronic_table;
-
+	vector <Item> item_table;
+	
+	vector <Item> items_in_cart;
+	vector <int> quantity;
+	
+	Database(void);
 	Database(string filename);
+	void set_directory(string directory);
 	vector<string> tokenize(const string &line);
 	void insert_user(string user);
-	void insert_household(Household item);
-	void insert_book(Book item);
-	void insert_toy(Toy item);
-	void insert_electronic(Electronic item);
+	void insert_item(Item item);
 
 	string generate_user(vector <string> values);
+	Item generate_item(vector <string> values);
 	Household generate_household(vector<string> values);
 	Book generate_book(vector<string> values);
 	Toy generate_toy(vector<string> values);
 	Electronic generate_electronic(vector<string> values);
 
+	vector< vector<string> > get_order_history(string user);
 	bool search_user(string value);
-	void Database::print_all();
+	void search_item(string value);
+	void print(string type);
+	void print_all();
+	void updateCartInDatabase(vector<Item> items,vector<int> quantity);
+	void addOrderToDatabase(string user, vector<Item> items,vector<int> quantity,float total,string address,string payment);
+	void getCartFromDatabase(vector<Item> &items,vector<int> &quantity);
 };
 
+Database::Database(void){}
+
 Database::Database(string directory) {
+	this->directory = directory;
 	// Input file that stores usernames
 	ifstream userInfile(directory + "/users.txt");
 	// Input file that stores item data in comma separated format.
@@ -54,14 +64,52 @@ Database::Database(string directory) {
 		// [0] is the data type
 		// [x-y] will be the following data, e.g. "name" "price" etc.
 
-		if (parts[0] == "household" && parts.size() == 8)
-			insert_household(generate_household(parts));
+		if (parts[0] == "item" && parts.size() == 5)
+			insert_item(generate_item(parts));
+		else if (parts[0] == "household" && parts.size() == 8)
+			insert_item(generate_household(parts));
 		else if (parts[0] == "book" && parts.size() == 8)
-			insert_book(generate_book(parts));
+			insert_item(generate_book(parts));
 		else if (parts[0] == "toy" && parts.size() == 7)
-			insert_toy(generate_toy(parts));
+			insert_item(generate_toy(parts));
 		else if (parts[0] == "electronic" && parts.size() == 7)
-			insert_electronic(generate_electronic(parts));
+			insert_item(generate_electronic(parts));
+	}
+}
+
+void Database::set_directory(string directory){
+	this->directory = directory;
+	// Input file that stores usernames
+	ifstream userInfile(directory + "/users.txt");
+	// Input file that stores item data in comma separated format.
+	ifstream itemInfile(directory + "/items.txt");
+	// A temporary string variable for parsing purposes.
+	string line;
+	char delimiter = ',';
+
+	// Initialize username list from file
+	while (getline(userInfile, line)) {
+		insert_user(line);
+	}
+
+	// Initialize items list from file.
+	while (getline(itemInfile, line)) {
+		vector <string> parts;
+		split(line, delimiter, parts);
+
+		// [0] is the data type
+		// [x-y] will be the following data, e.g. "name" "price" etc.
+
+		if (parts[0] == "item" && parts.size() == 5)
+			insert_item(generate_item(parts));
+		else if (parts[0] == "household" && parts.size() == 8)
+			insert_item(generate_household(parts));
+		else if (parts[0] == "book" && parts.size() == 8)
+			insert_item(generate_book(parts));
+		else if (parts[0] == "toy" && parts.size() == 7)
+			insert_item(generate_toy(parts));
+		else if (parts[0] == "electronic" && parts.size() == 7)
+			insert_item(generate_electronic(parts));
 	}
 }
 
@@ -82,41 +130,29 @@ void Database::insert_user(string user) {
 	user_table.push_back(user);
 }
 
-void Database::insert_household(Household item) {
-	household_table.push_back(item);
-}
-
-void Database::insert_book(Book item) {
-	book_table.push_back(item);
-}
-
-void Database::insert_toy(Toy item) {
-	toy_table.push_back(item);
-}
-
-void Database::insert_electronic(Electronic item) {
-	electronic_table.push_back(item);
+void Database::insert_item(Item item) {
+	item_table.push_back(item);
 }
 
 string Database::generate_user(vector <string> values) {
 	return string(values[1]);
 }
-
+Item Database::generate_item(vector <string> values) {
+	return Item(values[1], stof(values[2]), stoi(values[3]), values[4]);
+}
 Household Database::generate_household(vector<string> values) {
 	return Household(values[1], stof(values[2]), stoi(values[3]), values[4], values[5], values[6], values[7]);
 }
-
 Book Database::generate_book(vector<string> values) {
 	return Book(values[1], stof(values[2]), stoi(values[3]), values[4], values[5], values[6], values[7]);
 }
-
 Toy Database::generate_toy(vector<string> values) {
 	return Toy(values[1], stof(values[2]), stoi(values[3]), values[4], values[5], stoi(values[6]));
 }
-
 Electronic Database::generate_electronic(vector<string> values) {
 	return Electronic(values[1], stof(values[2]), stoi(values[3]), values[4], values[5], values[6]);
 }
+
 
 bool Database::search_user(string value) {
 	for (int i = 0; i != user_table.size(); i++) {
@@ -127,17 +163,54 @@ bool Database::search_user(string value) {
 	return false; // User does not exist.
 }
 
-void Database::print_all() {
-	for (int i = 0; i != household_table.size(); i++) {
-		household_table[i].print_details();
-	}
-	for (int i = 0; i != book_table.size(); i++) {
-		book_table[i].print_details();
-	}
-	for (int i = 0; i != toy_table.size(); i++) {
-		toy_table[i].print_details();
-	}
-	for (int i = 0; i != electronic_table.size(); i++) {
-		electronic_table[i].print_details();
+void Database::search_item(string value) {
+	for (int i = 0; i != item_table.size(); i++) {
+		if (item_table[i].name == value) {
+			item_table[i].print_details();
+		}
 	}
 }
+
+void Database::print(string type) {
+	for (int i = 0; i != item_table.size(); i++) {
+		if (item_table[i].type == type) {
+			item_table[i].print_details();
+		}
+	}
+}
+
+void Database::print_all() {
+	for (int i = 0; i != item_table.size(); i++) {
+		item_table[i].print_details();
+	}
+}
+
+void Database::updateCartInDatabase(vector<Item> items,vector<int> quantity){
+	
+}
+
+void Database::addOrderToDatabase(string user, vector<Item> items,vector<int> quantity,float total,string address,string payment){
+	
+}
+
+void Database::getCartFromDatabase(vector<Item> &items,vector<int> &quantity){
+	
+}
+
+//returns a vector of vectors containing info about the orders
+vector<vector<string>> Database::get_order_history(string user){
+	ifstream orderInfile(directory + "/" + user + ".txt");
+	// A temporary string variable for parsing purposes.
+	string line;
+	char delimiter = ',';
+	vector<vector<string>> orders;
+	
+	// Initialize items list from file.
+	while (getline(orderInfile, line)) {
+		vector <string> parts;
+		split(line, delimiter, parts);
+		orders.push_back(parts);
+	}
+	return orders;
+}
+
--- OnlineShopping/OnlineShopping/User.h
+++ OnlineShopping/OnlineShopping/User.h
@@ -9,17 +9,19 @@ public:
 	Cart cart;
 	string payment;
 	string address;
-	//Database dbManager;
+	Database dbManager;
+	vector<Order> orders;
 
 	User();
 	User(string name);
 	void Login(Database db);
 	void set_name(string n);
+	vector<Order> view_history();
 	void purchase_items();
-	void updateAddress(string newAddress);
-	void updatePayment(string newPayment);
-	void addToCart(Item item, int num);
-	void removeFromCart(Item item, int num);
+	void update_address(string newAddress);
+	void update_payment(string newPayment);
+	void add_to_cart(Item item, int num);
+	void remove_from_cart(Item item, int num);
 };
 
 User::User(void) { }
@@ -30,22 +32,41 @@ void User::set_name(string n) { this->name = n; }
 
 //stores the order in the database and empties the cart
 void User::purchase_items(){
-	//dbManager.addOrderToDatabase(name, cart.items, cart.quantity, cart.total, address, payment);
+	dbManager.addOrderToDatabase(name, cart.items, cart.quantity, cart.total, address, payment);
 	cart.clear();
 }
 
-void User::updateAddress(string newAddress){
+void User::update_address(string newAddress){
 	address = newAddress;
 }
 
-void User::updatePayment(string newPayment){
+void User::update_payment(string newPayment){
 	payment = newPayment;
 }
 
-void User::addToCart(Item item, int num){
+void User::add_to_cart(Item item, int num){
 	cart.addToCart(item, num);
 }
 
-void User::removeFromCart(Item item, int num){
+void User::remove_from_cart(Item item, int num){
 	cart.removeFromCart(item, num);
+}
+
+vector<Order> User::view_history(){
+	vector< vector<string> >  dbOrders = dbManager.get_order_history(name);
+	
+	for(int i=0;i<dbOrders.size();i++){
+		Order current_order;
+		current_order.payment = dbOrders[i][0];
+		current_order.address = dbOrders[i][1];
+		current_order.total = std::stof(dbOrders[i][2]);
+		for(int j=3;i<dbOrders.size();j+=3){
+			Item current_item;
+			current_item.name = dbOrders[i][j];
+			current_item.price = std::stof(dbOrders[i][j+2]);
+			current_order.quantity.push_back(std::stof(dbOrders[i][j+2]));
+			current_order.items.push_back(current_item);
+		}
+		orders.push_back(current_order);
+	}
 }
